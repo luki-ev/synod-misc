4d32bd9bf6efb92aef8978f1b714d9a8ba2c6b4d
Christoph Settgast <csett86@web.de>
2020-07-28 21:11:21 +0200

    Implement synapse compress
    All credits go to https://levans.fr/shrink-synapse-database.html

---
+++ scripts/synapse-compress.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+API_TOKEN=<ADD HERE>
+HOST=http://localhost:8008
+ROOMLIMIT=5000
+PSQL_USER=synapse
+PSQL_PASSWORD=password
+PSQL_HOST=localhost
+PSQL_DB=synapse
+
+ROOMLIST=$(curl --silent --header "Authorization: Bearer $API_TOKEN" $HOST/_synapse/admin/v1/rooms?limit=$ROOMLIMIT)
+
+ROOMS_WITHOUT_LOCAL_USERS=$(echo $ROOMLIST | jq '.rooms[] | select(.joined_local_members == 0) | .room_id')
+
+for room in $ROOMS_WITHOUT_LOCAL_USERS; do
+	curl --silent --header "Authorization: Bearer $API_TOKEN" --header "Content-Type: application/json" -d "{ \"room_id\": $room }" --output /dev/null $HOST/_synapse/admin/v1/purge_room
+done
+
+ROOMS_TO_COMPRESS=$(psql -w -U $PSQL_USER -h $PSQL_HOST --quiet -t -c 'SELECT room_id  FROM state_groups_state GROUP BY room_id HAVING count(*) > 100000;' $PSQL_DB)
+
+for room in $ROOMS_TO_COMPRESS; do
+	$HOME/bin/synapse-compress-state -t -o /tmp/state-compressor.sql -p "host=$PSQL_HOST user=$PSQL_USER password=$PSQL_PASSWORD dbname=$PSQ_DB" -r "$room" >/dev/null 2>&1
+    psql -w -U $PSQL_USER -h $PSQL_HOST --quiet $PSQL_DB < /tmp/state-compressor.sql
+done
+
+monit stop all
+
+psql -w -U $PSQL_USER -h $PSQL_HOST --quiet -t -c 'REINDEX DATABASE $PSQL_DB;' $PSQL_DB
+psql -w -U $PSQL_USER -h $PSQL_HOST --quiet -t -c 'VACUUM FULL;' $PSQL_DB >/dev/null 2>&1
+
+monit start all
